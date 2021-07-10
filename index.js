94d2917f307cfe67f0a54b570cee32920a6b66a4
thomas <thomasrcisneroz@gmail.com>
2021-07-09 18:31:30 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+document.getElementById('main').remove();
+
+const newHeader = document.createElement('h1');
+document.body.appendChild(newHeader);
+newHeader.id = 'victory';
+victory.innerHTML = 'TOM is the champion'
